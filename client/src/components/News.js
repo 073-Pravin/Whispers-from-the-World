824958eebb1bfda7e9cc57120b1b0653d63dfcde
PRAVIN PILODIYA <p143nimcet@gmail.com>
2024-03-13 00:47:03 +0530

    backend added

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,102 +1,107 @@
-import React, { useEffect ,useState } from "react";
+import React, { useEffect, useState } from "react";
 import NewsItems from "./NewsItems";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 import axios from "axios";
-const News = (props)=>{
-  
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
-  const [articles, setArticles]=useState([])
-  const [loading,setLoading]=useState(true)
-  const [page,setPage]=useState(1)
-  const [totalResults,setTotalResults]=useState(0)
+const News = (props) => {
+
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
   const CapitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  };
 
-  const updateNews =async ()=> {
+  const updateNews = async () => {
     props.setProgress(10);
     setLoading(true);
     props.setProgress(30);
-    let {data} = await axios.get(`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page+1}&pagesize=${props.pageSize}`);
-    console.log(data);
-    let parsedData=data;
-    
+    let { data } = await axios.get(
+      `/api/?country=${props.country}&category=${props.category}&apiKey=${
+        props.apikey
+      }&page=${page + 1}&pagesize=${props.pageSize}`
+    );
+    let parsedData = data;
+
     props.setProgress(70);
-    
+
     setArticles(parsedData.articles);
     setTotalResults(parsedData.totalResults);
     setLoading(false);
     props.setProgress(100);
-  }
-  useEffect(()=>{
-    document.title = `${CapitalizeFirstLetter(props.category)} - Whispers from the world`;
+  };
+  useEffect(() => {
+    document.title = `${CapitalizeFirstLetter(
+      props.category
+    )} - Whispers from the world`;
     updateNews();
-  },[])
+  }, []);
 
   const fetchMoreData = async () => {
     setLoading(true);
-  
-    let {data} = await axios.get(`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${page+1}&pagesize=${props.pageSize}`);
-    console.log(data);
-    let parsedData=data;
+
+    let { data } = await axios.get(
+      `/api/?country=${props.country}&category=${props.category}&apiKey=${
+        props.apikey
+      }&page=${page + 1}&pagesize=${props.pageSize}`
+    );
+    let parsedData = data;
     props.setProgress(70);
-    // console.log(parsedData);
-    setPage(page+1)
-    setArticles(articles.concat(parsedData.articles))
-    setTotalResults(parsedData.totalResults)
+    setPage(page + 1);
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
     props.setProgress(100);
     setLoading(false);
   };
 
-    return (
-      <>
-        <h1 className="text-center" style={{marginTop:73}}>
+  return (
+    <>
+      <h1 className="text-center" style={{ marginTop: 73 }}>
         Whispers from the World - Top {CapitalizeFirstLetter(props.category)}{" "}
-          Headlines
-        </h1>
-        {/* {loading && <Spinner />} */}
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalResults}
-          // hasMore={true}
-          loader={<Spinner/>}
-
-        >
-          <div className="container">
-            <div className="row">
-              {articles?.map((element) => {
-                return (
-                  <div className="col-md-4" key={element.url}>
-                    <NewsItems
-                      title={element.title ? element.title.slice(0, 45) : ""}
-                      description={
-                        element.description ? element.description : ""
-                      }
-                      imageUrl={element.urlToImage}
-                      newsUrl={element.url}
-                      author={element.author}
-                      date={element.publishedAt}
-                      source={element.source.name}
-                    />
-                  </div>
-                );
-              })}
-            </div>
+        Headlines
+      </h1>
+      {/* {loading && <Spinner />} */}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        // hasMore={true}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles?.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItems
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={element.description ? element.description : ""}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
           </div>
-        </InfiniteScroll>
-      </>
-    );
-  }
-export default News
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
+export default News;
 News.defaultProps = {
   country: "in",
   category: "general",
-}
+};
 News.propTypes = {
   category: PropTypes.string,
-  pageSize: PropTypes.number
-}
+  pageSize: PropTypes.number,
+};
---
+++ server/src/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the server");
+})
+
+app.get('/api',async (req,res)=>{
+    try {
+        console.log(req.query);
+        const { country, category, apiKey, page, pagesize  } = req.query;
+        const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pagesize=${pagesize}`
+        // const {url} = req.params;
+        console.log("URL: ",url);
+        const data = await fetch(`${url}`);
+        const jsonData = await data.json();
+        console.log(jsonData);
+        res.status(200).json(jsonData);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server listening on port ${PORT}`);
+})
